45d3573a2543e48c842fd8702c4af9fe6e72d062
niv9 <120386626+niv9@users.noreply.github.com>
2023-02-23 16:25:21 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+name_array=[];
+function submit(){
+var name1=document.getElementById("student1").value;
+var name2=document.getElementById("student2").value;
+var name3=document.getElementById("student3").value;
+var name4=document.getElementById("student4").value;
+
+name_array.push(name1);
+name_array.push(name2);
+name_array.push(name3);
+name_array.push(name4);
+
+console.log(name_array);
+document.getElementById("display_name").innerHTML=name_array;
+}
